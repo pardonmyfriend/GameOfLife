ea70b84d641129c47b72b2ba78aa1e5804f79058
pardonmyfriend <eva.kedziera@gmail.com>
2023-10-17 23:35:53 +0200

    gameoflife.py - wersja 2

--- gameoflife.py
+++ gameoflife.py
@@ -2,9 +2,8 @@ import time
 import pygame
 import numpy as np
 
-color_die_next = (200, 200, 225)
-color_alive_next = (255, 255, 215)
-color_background = (10, 10, 10)
+color_die = (10, 10, 10)
+color_alive = (255, 255, 215)
 color_grid = (40, 40, 40)
 
 
@@ -15,23 +14,23 @@ def update(screen, cells, size, with_progress=False):
         alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
 
         if cells[row, col] == 0:
-            color = color_background
+            color = color_die
         else:
-            color = color_alive_next
+            color = color_alive
 
         if cells[row, col] == 1:
             if alive < 2 or alive > 3:
                 if with_progress:
-                    color = color_die_next
+                    color = color_die
             elif 2 <= alive <= 3:
                 updated_cells[row, col] = 1
                 if with_progress:
-                    color = color_alive_next
+                    color = color_alive
         else:
             if alive == 3:
                 updated_cells[row, col] = 1
                 if with_progress:
-                    color = color_alive_next
+                    color = color_alive
 
         pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
 
@@ -40,12 +39,12 @@ def update(screen, cells, size, with_progress=False):
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((800, 600))
+    screen = pygame.display.set_mode((600, 400))
     pygame.display.set_caption("John Conway's Game of Life")
 
-    cells = np.zeros((60, 80))
+    cells = np.zeros((40, 60))
     screen.fill(color_grid)
-    update(screen, cells, 10)
+    update(screen, cells, 20)
 
     pygame.display.flip()
     pygame.display.update()
@@ -60,22 +59,21 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
-                    update(screen, cells, 10)
+                    update(screen, cells, 20)
                     pygame.display.update()
 
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
-                cells[pos[1] // 10, pos[0] // 10] = 1
-                update(screen, cells, 10)
+                cells[pos[1] // 20, pos[0] // 20] = 1
+                update(screen, cells, 20)
                 pygame.display.update()
 
         screen.fill(color_grid)
 
         if running:
-            cells = update(screen, cells, 10, with_progress=True)
+            cells = update(screen, cells, 20, with_progress=True)
             pygame.display.update()
-
-        time.sleep(0.1)
+            time.sleep(1)
 
 
 if __name__ == "__main__":
