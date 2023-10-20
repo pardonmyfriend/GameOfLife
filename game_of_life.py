b5bae05cd9731d88657d661075f4867d7017d03b
Zofia Sitek <zofia.a.sitek@gmail.com>
2023-10-20 18:29:49 +0200

    Lepsze kolorki

--- game_of_life.py
+++ game_of_life.py
@@ -22,7 +22,7 @@ class Board:
             x = 0
             for j in range(0, columns):
                 cell = []
-                state = random.randint(0, 1)
+                state = 0
                 cell.append(state)
                 cell.append(x)
                 cell.append(y)
@@ -37,7 +37,7 @@ class Board:
 
     def draw_window(self):
         win = pygame.display.set_mode((self.width, self.height))
-        grid = (0, 0, 0)
+        grid = (255, 255, 255)
         win.fill(grid)
         pygame.display.set_caption("Game Of Life")
         return win
@@ -45,8 +45,8 @@ class Board:
     def draw_cells(self, cells, win):
         self.width = cells[0][0][3]*self.columns
         self.height = cells[0][0][4]*self.rows
-        alive = (228, 208, 10)
-        dead = (128, 128, 128)
+        alive = (171, 218, 118)
+        dead = (122, 125, 118)
         for row in cells:
             for column in row:
                 if column[0] == 1:
@@ -73,14 +73,15 @@ class Board:
             for column in row:
                 new_column = column.copy()
                 alive_n_counter = 0
-                # check neighbours above
-                r = cells.index(row) + 1
-                if r >= 0 and r <= (self.rows - 1):
-                    for c in range(row.index(column) - 1, row.index(column) + 2, 1):
-                        if c >= 0 and c <= (self.columns - 1):
-                            state = cells[r][c][0]
-                            if state == 1:
-                                alive_n_counter += 1
+                # check neighbours above and under
+                for r in range(cells.index(row) - 1, cells.index(row) + 2, 2):
+                # r = cells.index(row) + 1
+                    if r >= 0 and r <= (self.rows - 1):
+                        for c in range(row.index(column) - 1, row.index(column) + 2, 1):
+                            if c >= 0 and c <= (self.columns - 1):
+                                state = cells[r][c][0]
+                                if state == 1:
+                                    alive_n_counter += 1
                 # check neighbours in the same row
                 r = cells.index(row)
                 for c in range(row.index(column) - 1, row.index(column) + 2, 2):
@@ -89,13 +90,13 @@ class Board:
                         if state == 1:
                             alive_n_counter += 1
                 # check neighbours under
-                r = cells.index(row) - 1
-                if r >= 0 and r <= (self.rows - 1):
-                    for c in range(row.index(column) - 1, row.index(column) + 2, 1):
-                        if c >= 0 and c <= (self.columns - 1):
-                            state = cells[r][c][0]
-                            if state == 1:
-                                alive_n_counter += 1
+                # r = cells.index(row) - 1
+                # if r >= 0 and r <= (self.rows - 1):
+                #     for c in range(row.index(column) - 1, row.index(column) + 2, 1):
+                #         if c >= 0 and c <= (self.columns - 1):
+                #             state = cells[r][c][0]
+                #             if state == 1:
+                #                 alive_n_counter += 1
                 c_state = column[0]
                 if c_state == 1:
                     if alive_n_counter != 2 and alive_n_counter != 3:
@@ -111,13 +112,13 @@ class Board:
         return cells
 
     def initialize(self):
-        self.width = 300
-        self.height = 300
+        self.width = 600
+        self.height = 600
         win = self.draw_window()
         start = False
         pygame.init()
         run = True
-        cells = self.generate_cells(10, 10)
+        cells = self.generate_cells(20, 20)
         self.draw_cells(cells, win)
         while run:
             for event in pygame.event.get():
